2a98c02ff4113db8e65d70a1f7465c677b5e4ead
FUFSoB <fufsob@fufsob.ru>
2022-04-29 17:57:04 +0500

    Initial grid

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button clicked = (Button)sender;
+            if (clicked.Name == "quit")
+                this.Close();
+            else if (clicked.Name == "settings")
+                new Settings().Show();
+            else if (clicked.Name == "game")
+            {
+                new Game().Show();
+                this.Close();
+            }
+        }
+    }
+}
